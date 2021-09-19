5b39b22d8b69c9457ee4e1a5dab5208449ccecfa
Meksich <maks10031003@gmail.com>
2021-09-19 17:33:43 +0300

    Add View Page for lab-3

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Landing Website For Laboratory work №1
+# Landing Website For Laboratory work №3
 Simple landing page made with pure HTML/CSS/JS for students of ["Internet of Things"](http://iot.lviv.ua) programm in Lviv Polytechnic National University.
 The website was made by students of [the same program](http://iot.lviv.ua), who decided to contribute into education and to inspire young generation 
 
 ---
-## Design
-[Click to view in Figma](https://www.figma.com/file/CKE7rSZvwWAckhsFpQLfZW/LabWork1-V1?node-id=0%3A1)
+## Task
+In this work, you have to make a simple presentation part of a website - View Page.
+Then using JavaScript, you need to implement the following operations on your data (it is up to you to decide which field should be used for each of the operations): 
+●	Sort of your items option
+●	Search option 
+●	Count total amount of some of the field 
+(e.g total price of all cars)
+
+Requirements:
+●	Responsiveness absolutely not required.
+●	Styling is not important at all. Is up to you.
 
 ## Installation
 You could just download the project and open **index.html** file and you are all set!<br>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,83 @@
-const OPEN_CLASSNAME = "open";
+import { renderItemsDOM, calculateTotal } from "./modules.js";
 
-const navLinks = document.getElementById("nav-links");
+const cardDeck = document.getElementById("card-deck");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const sortCheckbox = document.getElementById("sort");
+const countBtn = document.getElementById("count");
+const countResults = document.getElementById("count_results");
+const countTotal = document.getElementById("count_total");
 
-function toggleMenu() {
-  navLinks.classList.toggle(OPEN_CLASSNAME);
-}
+let ships = [
+    {
+        id: 1,
+        shipName: "Venator Star Ship",
+        tonnage: parseInt("8000000"),
+        capacity: parseInt("7400"),
+        image: "Venator-class-Star-Ship.jpg",
+    },
+    {
+        id: 2,
+        shipName: "Cosacks Submarine",
+        tonnage: parseInt("0"),
+        capacity: parseInt("5"),
+        image: "Cosacks_submarine.jpg",
+    },
+    {
+        id: 3,
+        shipName: "Republic Gunship(LAAT)",
+        tonnage: parseInt("30"),
+        capacity: parseInt("30"),
+        image: "Republic-gunship.jpg",
+    },
+    {
+        id: 4,
+        shipName: "Evergreen",
+        tonnage: parseInt("100000"),
+        capacity: parseInt("120"),
+        image: "Evergreen.jpg",
+    },
+    {
+        id: 5,
+        shipName: "Titanic",
+        tonnage: parseInt("46,328"),
+        capacity: parseInt("3320"),
+        image: "Titanic.jpg",
+    },
+    {
+        id: 6,
+        shipName: "Oxenfurt-Tretogor",
+        tonnage: parseInt("1258"),
+        capacity: parseInt("82"),
+        image: "Redanian-oxenfurt-tretogor.jpg",
+    },
+];
+let shipsSorted = [];
+
+searchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let shipsFound = ships.filter(
+        (ship) => ship.shipName.search(searchInput.value) !== -1);
+    renderItemsDOM(shipsFound);
+});
+
+sortCheckbox.addEventListener("change", () => {
+    let shipsSorted = Array.from(ships);
+    if (sortCheckbox.checked) {
+        shipsSorted.sort(
+            (first, second) => first.tonnage - second.tonnage
+        );
+    }
+    renderItemsDOM(shipsSorted);
+});
+
+countBtn.addEventListener("click", () => {
+    countResults.classList.remove("hidden");
+    const totalPrice = calculateTotal(ships, (ship) => ship.capacity);
+    countTotal.innerHTML = totalPrice;
+});
+
+renderItemsDOM(ships);
+
+export default ships;
+export { cardDeck };
